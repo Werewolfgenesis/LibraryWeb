0f5d288f6d9c2ea9699fd050df59659cccd33f0e
Sotirov <dimitar.sotirov@dxc.com>
2022-06-18 21:06:27 +0300

    added utility class for entity convertions

--- library-web/src/main/java/books/system/demo/convertions/Convertions.java
+++ library-web/src/main/java/books/system/demo/convertions/Convertions.java
@@ -1,2 +1,26 @@
-package books.system.demo.convertions;public class Convertions {
+package books.system.demo.convertions;
+
+import books.system.demo.dtos.BookDto;
+import books.system.demo.dtos.UserDto;
+import books.system.demo.model.Book;
+import books.system.demo.model.User;
+
+//Utility class to be used when converting objects.
+public class Convertions {
+    private Convertions() {
+        // No instances are to be made of this class.
+    }
+
+    public static UserDto convertUserToDto(User user) {
+        return new UserDto(user.getUsername(), user.getPassword(),
+                user.getNotes(), user.getCollectionBooks());
+    }
+
+    public static BookDto convertBookToDto(Book book){
+        return new BookDto(book.getTitle(), book.getISBN(), book.getGenre(),book.getAuthor());
+    }
+
+    public static Book convertDtoToBook(BookDto bookDto){
+        return new Book(bookDto.getISBN(), bookDto.getAuthor(), bookDto.getGenre(),bookDto.getTitle());
+    }
 }
