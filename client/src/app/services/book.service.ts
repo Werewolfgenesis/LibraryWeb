0805ef437cacf16a90507e116023e2e613419228
Atanas Ivanov <naskuuu123@gmail.com>
2022-06-19 15:05:41 +0300

    feat(client): add book service, fix delete user url

---
+++ client/src/app/services/book.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {Book} from "../model/Book";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private readonly http: HttpClient) {}
+
+  getUser(isbn: string): Observable<Book> {
+    return this.http.get<Book>(`${environment.restApi}/books/${isbn}`);
+  }
+
+  createBook(newBook: Book): Observable<Book> {
+    return this.http.post<Book>(`${environment.restApi}/books`, newBook);
+  }
+
+  updateBook(updatedBook: Book): Observable<Book> {
+    return this.http.put<Book>(`${environment.restApi}/books`, updatedBook);
+  }
+
+  deleteBook(isbn: string) {
+    return this.http.delete<Book>(`${environment.restApi}/books/${isbn}`);
+  }
+}
