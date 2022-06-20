3a90dce2a756d5dc2b6d196aa80f34c7a53b98ff
Sotirov <dimitar.sotirov@dxc.com>
2022-06-20 17:51:38 +0300

    using services

--- client/src/app/components/books-list/books-list.component.ts
+++ client/src/app/components/books-list/books-list.component.ts
@@ -1,42 +1,65 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import { Book } from "../../model/Book";
-// @ts-ignore
-import booksData from "../../../assets/mock-data.json";
+import { Component, HostListener, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
+import { BookService } from 'src/app/services/book.service';
+import { Book } from '../../model/Book';
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
-  styleUrls: ['./books-list.component.css']
+  styleUrls: ['./books-list.component.css'],
 })
 export class BooksListComponent implements OnInit {
   books: Book[];
-  displayedColumns: string[] = ['isbn', 'title', 'author', 'published', 'pages', 'description'];
+  displayedColumns: string[] = [
+    'isbn',
+    'title',
+    'author',
+    'published',
+    'pages',
+    'description',
+  ];
   clickedBooks = new Set<Book>();
 
   // TODO: think if we can separate this in another component
   searchText: string;
   previousBooks: string;
+  loading = false;
 
   @HostListener('input') oninput() {
     this.searchBooks();
   }
 
-  constructor() {
-  }
+  constructor(private readonly bookService: BookService) {}
 
   ngOnInit(): void {
-    this.books = booksData;
+    this.getAllBooks();
+  }
+
+  getAllBooks() {
+    this.loading = true;
+    this.bookService
+      .getAllBooks()
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe((response) => {
+        if (response) {
+          console.log(response);
+          this.books = response;
+        }
+      });
   }
 
   searchBooks(): void {
-    if (!this.searchText)
-      this.ngOnInit();
+    if (!this.searchText) this.ngOnInit();
 
-    this.books = this.books.filter(book => {
-
-        return book.isbn.toLowerCase().match(this.searchText.toLowerCase()) ||
-               book.title.toLowerCase().match(this.searchText.toLowerCase()) ||
-               book.author.toLowerCase().match(this.searchText.toLowerCase());
-    })
+    this.books = this.books.filter((book) => {
+      return (
+        book.isbn.toLowerCase().match(this.searchText.toLowerCase()) ||
+        book.title.toLowerCase().match(this.searchText.toLowerCase()) ||
+        book.author.firstName
+          .toLowerCase()
+          .match(this.searchText.toLowerCase()) ||
+        book.author.lastName.toLowerCase().match(this.searchText.toLowerCase())
+      );
+    });
   }
 }
