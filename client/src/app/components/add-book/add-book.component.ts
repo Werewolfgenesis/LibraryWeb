4caef505e8caee73125c8c278f56a9300c83b037
Sotirov <dimitar.sotirov@dxc.com>
2022-06-20 12:18:50 +0300

    added add book method

--- client/src/app/components/add-book/add-book.component.ts
+++ client/src/app/components/add-book/add-book.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from "@angular/material/dialog";
+import { MatDialogRef } from '@angular/material/dialog';
+import { finalize } from 'rxjs';
+import { Book } from 'src/app/model/Book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.css']
+  styleUrls: ['./add-book.component.css'],
 })
 export class AddBookComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<AddBookComponent>) { }
+  loading = false;
+  constructor(
+    private readonly dialogRef: MatDialogRef<AddBookComponent>,
+    private readonly bookService: BookService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
+    //this.addBook();
     this.dialogRef.close();
+  }
+
+  addBook(book: Book) {
+    this.loading = true;
+    this.bookService
+      .createBook(book)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe((response) => {
+        if (response) {
+          alert('Book successfully created: ' + response.title);
+        }
+      });
   }
 }
