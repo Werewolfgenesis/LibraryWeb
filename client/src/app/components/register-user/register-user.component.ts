6efb57e3884b7755c66160e6f4752226e632f876
Sotirov <dimitar.sotirov@dxc.com>
2022-06-24 12:28:44 +0300

    added registration component

---
+++ client/src/app/components/register-user/register-user.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { finalize } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-register-user',
+  templateUrl: './register-user.component.html',
+  styleUrls: ['./register-user.component.css'],
+})
+export class RegisterUserComponent implements OnInit {
+  passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.passwordRegex),
+  ]);
+
+  loading = false;
+  constructor(private readonly userService: UserService) {}
+
+  ngOnInit(): void {}
+
+  registerUser(username: string, password: string) {
+    this.loading = true;
+    this.userService
+      .createUser({
+        username: username,
+        password: password,
+        books: [],
+      })
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe();
+  }
+}
