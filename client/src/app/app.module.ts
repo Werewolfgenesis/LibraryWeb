829c31764dca98af414e9a75a1d4ade881d34908
Sotirov <dimitar.sotirov@dxc.com>
2022-06-27 10:34:32 +0300

    imported reactive forms module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,36 +1,41 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from "./material/material.module";
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { BooksListComponent } from './components/books-list/books-list.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ViewBookComponent } from './components/view-book/view-book.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
-import { HttpClientModule } from "@angular/common/http";
-import {RouterModule, Routes} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import { SearchComponent } from './components/search/search.component';
-import { BooksCardListComponent } from "./components/books-card-list/books-card-list.component";
-import { CanvasComponent } from './components/canvas/canvas.component';
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { AddNoteComponent } from './components/add-note/add-note.component';
+import { BooksCardListComponent } from './components/books-card-list/books-card-list.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { SearchComponent } from './components/search/search.component';
+import { ViewBookComponent } from './components/view-book/view-book.component';
+import { MaterialModule } from './material/material.module';
 
 const routes: Routes = [
   {
-    path: '', redirectTo: 'books', pathMatch: 'full'
+    path: '',
+    redirectTo: 'books',
+    pathMatch: 'full',
   },
   {
-    path: 'books', component: BooksListComponent
+    path: 'books',
+    component: BooksListComponent,
   },
   {
-    path: 'books-cards', component: BooksCardListComponent
+    path: 'books-cards',
+    component: BooksCardListComponent,
   },
   {
-    path: 'books/:isbn', component: ViewBookComponent
-  }
+    path: 'books/:isbn',
+    component: ViewBookComponent,
+  },
 ];
 
 @NgModule({
@@ -45,6 +50,7 @@ const routes: Routes = [
     SearchComponent,
     CanvasComponent,
     AddNoteComponent,
+    RegisterUserComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,11 +60,10 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     FormsModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
   ],
-  exports: [
-    RouterModule
-  ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
