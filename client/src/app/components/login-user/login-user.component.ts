832e78353addb3e510a7f330372b9d75035ad77d
Sotirov <dimitar.sotirov@dxc.com>
2022-06-30 07:18:41 +0300

    fixed login component to use login service

--- client/src/app/components/login-user/login-user.component.ts
+++ client/src/app/components/login-user/login-user.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
-import {UserService} from "../../services/user.service";
-import {finalize} from "rxjs";
+import { FormControl, Validators } from '@angular/forms';
+import { finalize } from 'rxjs';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
-  styleUrls: ['./login-user.component.css']
+  styleUrls: ['./login-user.component.css'],
 })
 export class LoginUserComponent implements OnInit {
-
   passwordRegex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
@@ -19,18 +18,14 @@ export class LoginUserComponent implements OnInit {
   ]);
 
   loading = false;
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly loginService: LoginService) {}
 
   ngOnInit(): void {}
 
   loginUser(username: string, password: string) {
     this.loading = true;
-    this.userService
-      .createUser({
-        username: username,
-        password: password,
-        books: [],
-      })
+    this.loginService
+      .login(username, password)
       .pipe(finalize(() => (this.loading = false)))
       .subscribe();
   }
